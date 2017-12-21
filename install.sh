95f98d835c25671a57131387cf9ccfd5cf2244dc
eboumendil <eboumendil@users.noreply.github.com>
2017-12-20 16:25:10 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-echo "nohup java -jar archive/build/libs/hello-karyon-rxnetty-all-0.1.0.jar &" >> /etc/rc.local
+echo "nohup /usr/bin/java -jar /home/ec2-user/archive/build/libs/hello-karyon-rxnetty-all-0.1.0.jar &" >> /etc/rc.local
